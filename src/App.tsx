70f89a14d3a09366a8352fa588d3451e0fd94d6b
anwar13101998 <anwar@wiseskulls.com>
2023-08-23 15:10:46 +0530

    Cghanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <img
+            width={400}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHoa1d5KlQ7n3dTO7_44cesi4w6d4oqQ0Q0pTwglo5kvPvZM9FGs3nGjYi4OMZJNvqBJ4&usqp=CAU"
+            alt=""
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
